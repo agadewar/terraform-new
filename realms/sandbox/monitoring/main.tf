b89d2fbd9cb3a7055322ad435ad0900ce85c0f82
Steve Ardis <steve.ardis@banyanhills.com>
2019-03-18 12:25:03 -0400

    TASK 8747 - Make Grafana "adminPassword" a tfvars secret

--- realms/sandbox/monitoring/main.tf
+++ realms/sandbox/monitoring/main.tf
@@ -103,7 +103,8 @@ resource "helm_release" "prometheus" {
   namespace  = "${local.namespace}"
   chart      = "stable/prometheus-operator"
   values = [
-    "${file("custom-values.yaml")}"
+    # "${file("custom-values.yaml")}"
+    "${data.template_file.custom_values.rendered}"
   ]
 
   set {
