0a74fce8dbd1d0910ecc2d32cb1ef0e82e2bbe35
Benjamin John <benjamin.john@banyanhills.com>
2019-05-13 19:52:43 -0400

    Added maven-repo Storage Account and File Share

--- realms/sandbox/components/jenkins-storage/main.tf
+++ realms/sandbox/components/jenkins-storage/main.tf
@@ -38,6 +38,7 @@ resource "azurerm_managed_disk" "jenkins_home" {
   }
 }
 
+// DELETE MAVEN MANAGED DISK IF THE STORAGE ACCOUNT IS WORKING
 resource "azurerm_managed_disk" "maven_repo" {
   name                 = "maven-repo-${var.realm}"
   location             = "${var.resource_group_location}"
@@ -54,4 +55,29 @@ resource "azurerm_managed_disk" "maven_repo" {
   lifecycle{
     prevent_destroy = "true"
   }
+}
+
+// STORAGE ACCOUNT IN PLACE OF MANAGED DISK TO ALLOW READ/WRITE-MANY
+// SET UP WITH TERRAFORM KUBERNETES_SECRET
+resource "azurerm_storage_account" "maven_repo" {
+  name                      = "mavenrepo${var.realm}"
+  resource_group_name       = "${var.resource_group_name}"
+  location                  = "${var.resource_group_location}"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+
+  tags = "${merge(
+    local.common_tags,
+    map()
+  )}"
+}
+
+resource "azurerm_storage_share" "maven_repo" {
+  name = "maven-repo"
+
+  resource_group_name       = "${var.resource_group_name}"
+  storage_account_name = "${azurerm_storage_account.maven_repo.name}"
+
+  quota = 20
 }
