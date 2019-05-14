cbab8ef43e3e84b911ef04d98fb638195a647ce2
Benjamin John <benjamin.john@banyanhills.com>
2019-05-14 15:10:07 -0400

    Removed data-lake Gen1 from Dev and Sandbox

--- realms/lab/environments/dev/data-lake/main.tf
+++ realms/lab/environments/dev/data-lake/main.tf
@@ -19,34 +19,45 @@ locals {
   )}"
 }
 
-resource "azurerm_data_lake_store" "sapience" {
-  name                = "sapience${var.environment}"
-  resource_group_name = "${var.resource_group_name}"
-  location            = "eastus2"
-  encryption_state    = "Enabled"
-  encryption_type     = "ServiceManaged"
 
-  tags = "${merge(
-    local.common_tags,
-    map()
-  )}"
-}
 
-resource "azurerm_data_lake_store_firewall_rule" "ardis_home" {
-  name                = "ardis-home"
-  account_name        = "${azurerm_data_lake_store.sapience.name}"
-  resource_group_name = "${var.resource_group_name}"
-  start_ip_address    = "24.99.117.169"
-  end_ip_address      = "24.99.117.169"
-}
 
-resource "azurerm_data_lake_store_firewall_rule" "banyan" {
-  name                = "banyan"
-  account_name        = "${azurerm_data_lake_store.sapience.name}"
-  resource_group_name = "${var.resource_group_name}"
-  start_ip_address    = "50.20.0.62"
-  end_ip_address      = "50.20.0.62"
-}
+
+
+# resource "azurerm_data_lake_store" "sapience" {
+#   name                = "sapience${var.environment}"
+#   resource_group_name = "${var.resource_group_name}"
+#   location            = "eastus2"
+#   encryption_state    = "Enabled"
+#   encryption_type     = "ServiceManaged"
+
+#   tags = "${merge(
+#     local.common_tags,
+#     map()
+#   )}"
+# }
+
+# resource "azurerm_data_lake_store_firewall_rule" "ardis_home" {
+#   name                = "ardis-home"
+#   account_name        = "${azurerm_data_lake_store.sapience.name}"
+#   resource_group_name = "${var.resource_group_name}"
+#   start_ip_address    = "24.99.117.169"
+#   end_ip_address      = "24.99.117.169"
+# }
+
+# resource "azurerm_data_lake_store_firewall_rule" "banyan" {
+#   name                = "banyan"
+#   account_name        = "${azurerm_data_lake_store.sapience.name}"
+#   resource_group_name = "${var.resource_group_name}"
+#   start_ip_address    = "50.20.0.62"
+#   end_ip_address      = "50.20.0.62"
+# }
+
+
+
+
+
+
 
 resource "null_resource" "azure_data_lake_storage_gen2" {
   # triggers = {
