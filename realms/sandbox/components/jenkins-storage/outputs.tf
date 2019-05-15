e77fdb11b018dcf341c8c0f92156d8c1cf695a7d
Benjamin John <benjamin.john@banyanhills.com>
2019-05-15 16:10:03 -0400

    Removed Maven Repo Azure Disk

--- realms/sandbox/components/jenkins-storage/outputs.tf
+++ realms/sandbox/components/jenkins-storage/outputs.tf
@@ -2,10 +2,6 @@ output "jenkins_home_disk_id" {
   value       = "${azurerm_managed_disk.jenkins_home.id}"
 }
 
-output "maven_repo_disk_id" {
-  value       = "${azurerm_managed_disk.maven_repo.id}"
-}
-
 output "maven_repo_storage_account_access_key" {
   description = "The access key for the storage account"
   value       = "${azurerm_storage_account.maven_repo.primary_access_key}"
