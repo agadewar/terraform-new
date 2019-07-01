a8fd205ce6e1a0041661bb6ce866ed4b6d72ccac
Mason Huemmer <Mason.Huemmer@sapience.net>
2019-07-01 08:10:01 -0500

    added CGP-Sandbox

---
+++ realms/CGP-Sandbox/components/network/variables.tf
@@ -0,0 +1,14 @@
+
+variable "subscription_id" {}
+variable "resource_group_name" {}
+variable "resource_group_location" {}
+variable "virtual_network_address_space" {
+  type = "list" 
+}
+variable "subnet_address_prefix" {}
+variable "subnet_service_endpoints" {
+  type = "list"
+}
+variable "realm_common_tags" {
+  type = "map"
+}
---
+++ realms/CGP-Sandbox/components/sapience-server/variables.tf
@@ -0,0 +1,12 @@
+variable "subscription_id" {}
+
+variable "resource_group_name" {}
+variable "resource_group_location" {}
+
+variable "admin_username" {}
+
+variable "admin_password" {}
+
+variable "realm_common_tags" {
+  type = "map"
+}
