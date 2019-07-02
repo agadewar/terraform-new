03769aa8d8220300ff62d9d59f1fd5852cf648b8
Steve Ardis <steve.ardis@banyanhills.com>
2019-07-01 22:23:11 -0400

    PBI-13392 - Removed eventpipeline-registry from "canopy" module

--- realms/lab/environments/dev/canopy/main.tf
+++ realms/lab/environments/dev/canopy/main.tf
@@ -674,77 +674,77 @@ module "eventpipeline_service" {
   )}"
 }
 
-resource "kubernetes_config_map" "eventpipeline_registry" {
-  metadata {
-    name      = "eventpipeline-registry"
-    namespace = "${local.namespace}"
-  }
+# resource "kubernetes_config_map" "eventpipeline_registry" {
+#   metadata {
+#     name      = "eventpipeline-registry"
+#     namespace = "${local.namespace}"
+#   }
 
-  data {
-    "global.properties"      = "${data.template_file.global_properties.rendered}"
-  }
-}
+#   data {
+#     "global.properties"      = "${data.template_file.global_properties.rendered}"
+#   }
+# }
 
-resource "kubernetes_secret" "eventpipeline_registry" {
-  metadata {
-    name      = "eventpipeline-registry"
-    namespace = "${local.namespace}"
-  }
+# resource "kubernetes_secret" "eventpipeline_registry" {
+#   metadata {
+#     name      = "eventpipeline-registry"
+#     namespace = "${local.namespace}"
+#   }
 
-  data {
-  }
+#   data {
+#   }
 
-  type = "Opaque"
-}
+#   type = "Opaque"
+# }
 
-module "eventpipeline_registry" {
-  source = "../../../../../../terraform-canopy-service-module/"
+# module "eventpipeline_registry" {
+#   source = "../../../../../../terraform-canopy-service-module/"
 
-  kubeconfig_path = "${local.config_path}"
+#   kubeconfig_path = "${local.config_path}"
 
-  name      = "eventpipeline-registry"
-  namespace = "${local.namespace}"
+#   name      = "eventpipeline-registry"
+#   namespace = "${local.namespace}"
 
-  deployment_image             = "${var.sapience_container_registry_hostname}/eventpipeline-registry:1.0.0-SNAPSHOT"
-  deployment_replicas          = 1
-  deployment_image_pull_policy = "Always"
-  deployment_image_pull_secret_name = "${kubernetes_secret.sapience_container_registry_credential.metadata.0.name}"   # don't use the local value string here... we need a dependency on the secret being created
+#   deployment_image             = "${var.sapience_container_registry_hostname}/eventpipeline-registry:1.0.0-SNAPSHOT"
+#   deployment_replicas          = 1
+#   deployment_image_pull_policy = "Always"
+#   deployment_image_pull_secret_name = "${kubernetes_secret.sapience_container_registry_credential.metadata.0.name}"   # don't use the local value string here... we need a dependency on the secret being created
 
-  resources = [
-    {
-      requests = [
-        {
-          memory = "256M"
-          cpu = "10m"
-        }
-      ]
-    }
-  ]
+#   resources = [
+#     {
+#       requests = [
+#         {
+#           memory = "256M"
+#           cpu = "10m"
+#         }
+#       ]
+#     }
+#   ]
 
-  default_token = "${data.terraform_remote_state.kubernetes_namespace.default_token_secret_name}"
+#   default_token = "${data.terraform_remote_state.kubernetes_namespace.default_token_secret_name}"
 
-  deployment_env = []
+#   deployment_env = []
 
-  service_spec = [
-    {
-      // TODO (PBI-12532) - once "terraform-provider-kubernetes" commit "4fa027153cf647b2679040b6c4653ef24e34f816" is merged, change the prefix on the
-      //                    below labels to "app.kubernetes.io" - see: https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
-      selector {
-        "sapienceanalytics.com/name" = "eventpipeline-registry"
-      }
+#   service_spec = [
+#     {
+#       // TODO (PBI-12532) - once "terraform-provider-kubernetes" commit "4fa027153cf647b2679040b6c4653ef24e34f816" is merged, change the prefix on the
+#       //                    below labels to "app.kubernetes.io" - see: https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
+#       selector {
+#         "sapienceanalytics.com/name" = "eventpipeline-registry"
+#       }
 
-      port = [
-        {
-          name        = "application"
-          port        = 80
-          target_port = 8080
-        }
-      ]
-    }
-  ]
+#       port = [
+#         {
+#           name        = "application"
+#           port        = 80
+#           target_port = 8080
+#         }
+#       ]
+#     }
+#   ]
 
-  labels = "${merge(
-    local.common_labels,
-    map()
-  )}"
-}
+#   labels = "${merge(
+#     local.common_labels,
+#     map()
+#   )}"
+# }
