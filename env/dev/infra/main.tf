b137ba4f8dbe4f36f3b92296577637c2c8e46364
Mason Huemmer <Mason.Huemmer@sapienceanalytics.com>
2020-03-18 16:10:11 -0500

    update files

--- env/dev/infra/main.tf
+++ env/dev/infra/main.tf
@@ -1,24 +1,14 @@
 # -------------------------------------------------------------------------------
-# REMOTE TFSTATE FILE
-# -------------------------------------------------------------------------------
-
-terraform {
-  backend "azurerm" {
-    key = "${var.environment}-${var.location}.tfstate"
-  }
-}
-
-# -------------------------------------------------------------------------------
 # Providers
 # -------------------------------------------------------------------------------
 
 provider "azurerm" {
-  version         = "1.37.0"
   
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+
 }
 
 # -------------------------------------------------------------------------------
@@ -26,9 +16,8 @@ provider "azurerm" {
 # - Resource Group
 # -------------------------------------------------------------------------------
 
-module "resource-group" {
-  source          = "../modules/resource-group"
+module "resourcegroup" {
+  source  = "app.terraform.io/sapience-analytics/resourcegroup/azurerm"
 
   environment     = var.environment
-  location        = var.location
 }
--- env/dev/infra/variables.tf
+++ env/dev/infra/variables.tf
@@ -3,18 +3,13 @@
 # You must provide a value for each of these parameters.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "tfstate_container_name" {
-  description = "Create/Update Container in blob storage"
-  type        = string
-}
-
-variable "tfstate_access_key" {
-  description = "Access key to blob storage in production"
+variable "tenant_id" {
+  description = "Azure Tenant ID"
   type        = string
 }
 
 variable "subscription_id" {
-  description = "Azure Subscription"
+  description = "Azure Subscription ID"
   type        = string
 }
 
@@ -28,18 +23,8 @@ variable "client_secret" {
   type        = string
 }
 
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
 variable "environment" {
   description = "The environment the resource is deployed"
-  type        = string
-}
-
-variable "location" {
-  description = "The location where the resource is deployed"
   type        = string
 }
 
@@ -48,14 +33,8 @@ variable "location" {
 # These parameters have reasonable defaults.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "tfstate_resource_group" {
-  description = "The environment the resource is deployed"
+variable "location" {
+  description = "The location where the resource is deployed"
   type        = string
-  default     = "devops"
-}
-
-variable "tfstate_storage_account" {
-  description = "Blob storage in production"
-  type        = string
-  default     = "sapienceremotestate"
+  default     = "eastus"
 }
