4e3cde21470b955ed2325f014973a2828efd07a7
Mason Huemmer <Mason.Huemmer@sapienceanalytics.com>
2020-03-19 01:48:24 -0500

    update module versions to support implicit dependency

--- env/dev/infra/main.tf
+++ env/dev/infra/main.tf
@@ -25,9 +25,9 @@ provider "azurerm" {
 # Resource Group
 # -------------------------------------------------------------------------------
 
-/* module "resourcegroup" {
+module "resourcegroup" {
   source  = "app.terraform.io/sapience-analytics/resourcegroup/azurerm"
-  version = "1.0.0"
+  version = "1.0.2"
 
   environment     = var.environment
 }
@@ -38,10 +38,10 @@ provider "azurerm" {
 
 module "storageaccount" {
   source  = "app.terraform.io/sapience-analytics/storageaccount/azurerm"
-  version = "1.0.2"
+  version = "1.0.3"
 
   environment     = var.environment
-}
+  resource_group  = module.resourcegroup.name
 
 # -------------------------------------------------------------------------------
 # Virtual Network
@@ -49,9 +49,10 @@ module "storageaccount" {
 
 module "network" {
   source  = "app.terraform.io/sapience-analytics/network/azurerm"
-  version = "1.0.1"
+  version = "1.0.2"
 
   environment                                          = var.environment
+  resource_group                                       = module.resourcegroup.name
   virtual_network_address_space                        = [ "10.1.0.0/16" ]
   subnet_default_address_prefix                        = "10.1.0.0/20"
   subnet_application_address_prefix                    = "10.1.16.0/20"
@@ -67,9 +68,10 @@ module "network" {
 
 module "loganalyticsworkspace" {
   source  = "app.terraform.io/sapience-analytics/loganalyticsworkspace/azurerm"
-  version = "1.0.0"
+  version = "1.0.1"
 
   environment     = var.environment
+  resource_group  = module.resourcegroup.name
 }
 
 # -------------------------------------------------------------------------------
@@ -78,9 +80,10 @@ module "loganalyticsworkspace" {
 
 module "kubernetes-stateless-black" {
   source  = "app.terraform.io/sapience-analytics/kubernetes/azurerm"
-  version = "1.0.3"
+  version = "1.0.4"
 
   environment     = var.environment
+  resource_group  = module.resourcegroup.name
   client_id       = var.client_id
   client_secret   = var.client_secret
   vnet_subnet_id  = module.network.aks_stateless_default_pool_subnet_id
@@ -93,11 +96,12 @@ module "kubernetes-stateless-black" {
 module "kubernetes-stateful-black" {
 
   source  = "app.terraform.io/sapience-analytics/kubernetes/azurerm"
-  version = "1.0.3"
+  version = "1.0.4"
 
   environment     = var.environment
+  resource_group  = module.resourcegroup.name
   client_id       = var.client_id
   client_secret   = var.client_secret
   vnet_subnet_id  = module.network.aks_stateful_default_pool_subnet_id
   state           = "stateful"
-} */
+} 
