981f8d055091ecb553db90fbb1040ed08c9462a5
Mason Huemmer <Mason.Huemmer@sapienceanalytics.com>
2020-03-20 00:45:44 -0500

    update module version for cosmosdb

--- env/dev/infra/main.tf
+++ env/dev/infra/main.tf
@@ -221,7 +221,7 @@ module "mysql" {
 
 module "cosmosdb" {
   source  = "app.terraform.io/sapience-analytics/cosmosdb/azurerm"
-  version = "1.0.4"
+  version = "1.0.5"
 
   environment               = var.environment
   resource_group            = module.resourcegroup.name
