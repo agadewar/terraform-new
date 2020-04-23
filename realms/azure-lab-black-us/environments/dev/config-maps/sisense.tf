8981d9339ea36f8894f6e5eb85a7edcccdebfc49
Ashish Gadewar <ashish.gadewar@sapienceanalytics.com>
2020-04-23 18:44:34 +0530

    Adding env vars for admin UI and changing data source for sisense

--- realms/azure-lab-black-us/environments/dev/config-maps/sisense.tf
+++ realms/azure-lab-black-us/environments/dev/config-maps/sisense.tf
@@ -9,7 +9,7 @@ resource "kubernetes_config_map" "sisense" {
       "Sisense__Env" = "Dev"
 
       #SISENSE DATASOURCES
-      "Sisense__DataSource" = "Sapience-Daily-CompanyId-Env"
+      "Sisense__DataSource" = "Set/Sapience-Daily-CompanyId-Env"
       "Sisense_DailyDataSource" = "Sapience-Daily-CompanyId-Env"
       "Sisense_HourlyDataSource" = "Sapience-Hourly-CompanyId-Env"
 
