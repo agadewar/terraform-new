71d4843ad96cfc5ef1c50a8bf21928ab705c0e3b
Ashish Gadewar <ashish.gadewar@sapienceanalytics.com>
2020-04-23 19:56:16 +0530

    updating config map auth0

--- realms/azure-lab-black-us/environments/qa/config-maps/auth0.tf
+++ realms/azure-lab-black-us/environments/qa/config-maps/auth0.tf
@@ -17,7 +17,10 @@ resource "kubernetes_config_map" "auth0" {
       "ENVIRONMENT_AUTH_AUDIENCE" = "https://api.sapienceanalytics.com"
       "ENVIRONMENT_AUTH_CLIENT_ID" = "ZxBrWiPNzzpjMA2zJep8BxLz9zQCmWDo"
       "ENVIRONMENT_AUTH_SCOPE" = "openid email profile"
-
+      "ENVIRONMENT_VUE_URL"    =  "https://app.qa.sapienceanalytics.com"
+      "ENVIRONMENT_ADMIN_URL"  =  "https://manage.qa.sapienceanalytics.com"
+      "auth0_alertrules_id"    = "RqqCr8ymt7csKTgDPrX3braxHve0kEhs"
+ 
       #AUTH0 URI
       "Auth0__PingUri" = "https://qa-sapienceanalytics.auth0.com/test"
 
