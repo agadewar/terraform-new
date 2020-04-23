43e7ed1a6f1606f2846b79d208de2b25d198794f
Ashish Gadewar <ashish.gadewar@sapienceanalytics.com>
2020-04-23 21:20:41 +0530

    Adding literals in misc for admin UI

--- realms/azure-lab-black-us/environments/qa/config-maps/misc.tf
+++ realms/azure-lab-black-us/environments/qa/config-maps/misc.tf
@@ -5,9 +5,11 @@ resource "kubernetes_config_map" "misc" {
   }
 
   data = {
-      "ASPNETCORE_ENVIRONMENT" = "Development"
-      "ENVIRONMENT_API_URL" = "https://api.qa.lab.us.azure.sapienceanalytics.com"
-      "LocalSwagger" = "false"
-      "API_URL" = "http://sapience-app-alerts/alertrules/company/eval"
+      "ASPNETCORE_ENVIRONMENT"    = "Development"
+      "ENVIRONMENT_API_URL"       = "https://api.qa.lab.us.azure.sapienceanalytics.com"
+      "LocalSwagger"              = "false"
+      "API_URL"                   = "http://sapience-app-alerts/alertrules/company/eval"
+      "ENVIRONMENT_VUE_URL"       = "https://app.qa.sapienceanalytics.com"
+      "ENVIRONMENT_ADMIN_URL"    = "https://manage.qa.sapienceanalytics.com"
   }
 }
