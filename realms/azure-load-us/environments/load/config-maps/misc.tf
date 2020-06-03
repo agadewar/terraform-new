21761aff71948858f51790c3681d90c83a2dc455
Ashish Gadewar <ashish.gadewar@sapienceanalytics.com>
2020-06-03 11:45:29 +0530

    PBI-35674

--- realms/azure-load-us/environments/load/config-maps/misc.tf
+++ realms/azure-load-us/environments/load/config-maps/misc.tf
@@ -10,6 +10,7 @@ resource "kubernetes_config_map" "misc" {
       "ENVIRONMENT_ADMIN_URL"  =   "https://manage.load.sapienceanalytics.com"
       "ENVIRONMENT_VUE_URL"      =  "https://app.load.sapienceanalytics.com"
       "ENVIRONMENT_VUE_HELP_URL"    =   "https://help.load.load.us.azure.sapienceanalytics.com"
+      "ENVIRONMENT_CANOPY_URL"   =  "https://canopy.load.load.us.azure.sapienceanalytics.com/#/sso?path=%2FSapience"
       "LocalSwagger" = "false"
   }
 }
