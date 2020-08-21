5f44a898ebb3aafb3785ef875ee3ffbf94581e0d
Ashish Gadewar <ashishgadewar@AshishGMacBook-Pro.local>
2020-08-21 17:31:47 +0530

    adding event-hub support

---
+++ realms/azure-load-us/environments/load/event-hub/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  backend "azurerm" {
+    key = "event-hub.tfstate"
+  }
+}
+
+provider "azurerm" {
+  version         = "1.31.0"
+  subscription_id = var.subscription_id
+}
+
+
+locals {
+  common_tags = merge(
+    var.realm_common_tags,
+    var.environment_common_tags,
+    {
+      "Component" = "Service Bus"
+    },
+  )
+}
+
+
+resource "azurerm_eventhub_namespace" "namespace" {
+  name                = "sapience-eventhub-${var.realm}-${var.environment}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+  kafka_enabled       = true
+  capacity            = 1
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_eventhub" "canopy-eventpipeline" {
+  name                = "canopy-eventpipeline"
+  namespace_name      = azurerm_eventhub_namespace.namespace.name
+  resource_group_name = var.resource_group_name
+  partition_count     = 2
+  message_retention   = 1
+}
+
+resource "azurerm_eventhub" "eventarchive-channel" {
+  name                = "eventarchive-channel"
+  namespace_name      = azurerm_eventhub_namespace.namespace.name
+  resource_group_name = var.resource_group_name
+  partition_count     = 2
+  message_retention   = 1
+}
