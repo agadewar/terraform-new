5eb565602c20ff0908f81156aeb23b1638ed595a
Ashish Gadewar <ashishgadewar@AshishGMacBook-Pro.local>
2020-09-16 14:18:59 +0530

    adding mappings

--- realms/azure-lab-black-us/environments/dev/ambassador/main.tf
+++ realms/azure-lab-black-us/environments/dev/ambassador/main.tf
@@ -342,7 +342,7 @@ cors:
 ---
 apiVersion: ambassador/v1
 kind:  Mapping
-name:  admin-uploads-api_mapping
+name:  admin_uploads_api_mapping
 prefix: /admin/uploads/
 service: admin-uploads-api
 rewrite: /admin/uploads/
