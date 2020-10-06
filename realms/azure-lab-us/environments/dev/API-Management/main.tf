0dc602f7f51e3befd3bbbd73fd679cad904fe9b1
Sahil Davawala <sahil.davawala@sapienceanalytics.com>
2020-10-06 14:52:43 +0530

    Adding the APIM terraform manifests for all the environments at correct place.

---
+++ realms/azure-lab-us/environments/dev/API-Management/main.tf
@@ -0,0 +1,20 @@
+terraform {
+    backend "azurerm" {
+      key = "api-management.tfstate"
+    }
+  }
+  
+  provider "azurerm" {
+    version         = "1.31.0"
+    subscription_id = var.subscription_id
+  }
+
+locals {
+  common_tags = merge(
+    var.realm_common_tags,
+    var.environment_common_tags,
+    {
+      "Component" = "api-management"
+    },
+  )
+}
