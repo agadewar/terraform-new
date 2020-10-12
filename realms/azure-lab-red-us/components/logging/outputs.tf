f49616de3330b0bcbf57e62aadc0635ef9721133
Jagdish Goswami <jagdish.goswami@sapienceanalytics.com>
2020-10-12 10:07:41 +0530

    Added new template and tf files for kibana logging namespace

--- realms/azure-lab-red-us/components/logging/outputs.tf
+++ realms/azure-lab-red-us/components/logging/outputs.tf
@@ -1,19 +1,19 @@
-output "log_analytics_workspace_id" {
-  value = azurerm_log_analytics_workspace.log_analytics_workspace.id
-}
+# output "log_analytics_workspace_id" {
+#   value = azurerm_log_analytics_workspace.log_analytics_workspace.id
+# }
 
-output "log_analytics_workspace_primary_shared_key" {
-  value = azurerm_log_analytics_workspace.log_analytics_workspace.primary_shared_key
-}
+# output "log_analytics_workspace_primary_shared_key" {
+#   value = azurerm_log_analytics_workspace.log_analytics_workspace.primary_shared_key
+# }
 
-output "log_analytics_workspace_secondary_shared_key" {
-  value = azurerm_log_analytics_workspace.log_analytics_workspace.secondary_shared_key
-}
+# output "log_analytics_workspace_secondary_shared_key" {
+#   value = azurerm_log_analytics_workspace.log_analytics_workspace.secondary_shared_key
+# }
 
-output "log_analytics_workspace_workspace_id" {
-  value = azurerm_log_analytics_workspace.log_analytics_workspace.workspace_id
-}
+# output "log_analytics_workspace_workspace_id" {
+#   value = azurerm_log_analytics_workspace.log_analytics_workspace.workspace_id
+# }
 
-output "log_analytics_workspace_portal_url" {
-  value = azurerm_log_analytics_workspace.log_analytics_workspace.portal_url
-}
+# output "log_analytics_workspace_portal_url" {
+#   value = azurerm_log_analytics_workspace.log_analytics_workspace.portal_url
+# }
