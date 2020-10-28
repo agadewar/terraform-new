88fb67887d8fa70ff98ba8f5ca84c0cd85c8e330
Sahil Davawala <sahil.davawala@sapienceanalytics.com>
2020-10-28 15:58:51 +0530

    Updating the CM for all environments and service bus changes for Prod env

--- realms/azure-lab-red-us/environments/qa/config-maps/users.tf
+++ realms/azure-lab-red-us/environments/qa/config-maps/users.tf
@@ -9,9 +9,9 @@ resource "kubernetes_config_map" "users" {
       "AzureServiceBus__EntityPath"            = "sapience-admin-users-created"
       "AzureServiceBus__Endpoint"              = "Endpoint=sb://sapience-lab-us-qa.servicebus.windows.net/;SharedAccessKeyName=Publish;SharedAccessKey=x9D7XYbxSiMMVssulaXMBn4Q12u7SEHX1pqAkAToGJQ="
       "AzureServiceBus__EditEntityPath"        = "sapience-admin-users-updated"
-      "AzureServiceBus__EditEndpoint"          = "Endpoint=sb://sapience-lab-us-dev.servicebus.windows.net/;SharedAccessKeyName=Full;SharedAccessKey=1f8hCZFMPffFG7TUtv+JWMV94F+RgY1I3dgmlRh3xKc="
+      "AzureServiceBus__EditEndpoint"          = "Endpoint=sb://sapience-lab-us-qa.servicebus.windows.net/;SharedAccessKeyName=Full;SharedAccessKey=1f8hCZFMPffFG7TUtv+JWMV94F+RgY1I3dgmlRh3xKc="
       "AzureServiceBus__DeleteEntityPath"      = "sapience-admin-users-deleted"
-      "AzureServiceBus__DeleteEndpoint"        = "Endpoint=sb://sapience-lab-us-dev.servicebus.windows.net/;SharedAccessKeyName=Full;SharedAccessKey=dOB3YaQvCn6JTveBMen29pr9Ugk9o3h3X50tYGiI6aw="
+      "AzureServiceBus__DeleteEndpoint"        = "Endpoint=sb://sapience-lab-us-qa.servicebus.windows.net/;SharedAccessKeyName=Full;SharedAccessKey=dOB3YaQvCn6JTveBMen29pr9Ugk9o3h3X50tYGiI6aw="
 
   }
 }
