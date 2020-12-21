50b8255525b3bb4397213b73d3c9bf931a5c925c
Ashish Gadewar <ashish.gadewar@sapienceanalytics.com>
2020-12-21 19:36:52 +0530

    correcting users cm prod

--- realms/azure-prod-us-black/environments/prod/config-maps/users.tf
+++ realms/azure-prod-us-black/environments/prod/config-maps/users.tf
@@ -5,9 +5,9 @@ resource "kubernetes_config_map" "users" {
   }
 
   data = {
-      "AzureServiceBus__UseAzureServiceBus"    =  false
+      "AzureServiceBus__UseAzureServiceBus"    =  true
       "AzureServiceBus__EntityPath"            = "sapience-admin-users-created"
-      "AzureServiceBus__Endpoint"              = "Endpoint=sb://sapience-prod-us-prod.servicebus.windows.net/;SharedAccessKeyName=Publish;SharedAccessKey=5qUDVqkfHhmGS+KYIAnuvYEMU3N+CrcxiUhfI8t9kT0"
+      "AzureServiceBus__Endpoint"              = "Endpoint=sb://sapience-prod-us-prod.servicebus.windows.net/;SharedAccessKeyName=Publish;SharedAccessKey=5qUDVqkfHhmGS+KYIAnuvYEMU3N+CrcxiUhfI8t9kT0="
       "AzureServiceBus__EditEntityPath"        = "sapience-admin-users-updated"
       "AzureServiceBus__EditEndpoint"          = "Endpoint=sb://sapience-prod-us-prod.servicebus.windows.net/;SharedAccessKeyName=Full;SharedAccessKey=RTMP/wTWoL4TftZV8m9cHcwwT3yhGlt2auFw/vEoAVc="
       "AzureServiceBus__DeleteEntityPath"      = "sapience-admin-users-deleted"
