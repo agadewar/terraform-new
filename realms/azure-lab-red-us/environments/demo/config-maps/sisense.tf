082172237f1a20110ea0b12d32fcb982800054ce
Ashish Gadewar <ashish.gadewar@sapienceanalytics.com>
2021-01-21 17:17:56 +0530

    demo sisense linux changes

--- realms/azure-lab-red-us/environments/demo/config-maps/sisense.tf
+++ realms/azure-lab-red-us/environments/demo/config-maps/sisense.tf
@@ -10,6 +10,7 @@ resource "kubernetes_config_map" "sisense" {
 
       #SISENSE DATASOURCES
       "Sisense__DataSource" = "Set/Sapience-Daily-CompanyId-Env"
+      #"Sisense__DataSource" = "Sapience-Daily-CompanyId-Env"
       "Sisense_DailyDataSource" = "Sapience-Daily-CompanyId-Env"
       "Sisense_HourlyDataSource" = "Sapience-Hourly-CompanyId-Env"
 
@@ -22,6 +23,7 @@ resource "kubernetes_config_map" "sisense" {
       "Sisense_PingUri" = "api/test"
       #UPDATE REQUEST URI (e.g. SapienceCube-[Env])
       "Sisense__RequestUri" = "api/elasticubes/SapienceCube-Demo/jaql"
+      #"Sisense__RequestUri" = "api/datasources/SapienceCube-Dev/jaql"
       "Sisense_SearchUserUri" = "api/users?search="
       "Sisense_SecurityUri" = "api/settings/security"
       "Sisense_UsersUri" = "api/users?notify=false"
@@ -29,13 +31,23 @@ resource "kubernetes_config_map" "sisense" {
 
       #SISENSE EXTERNAL ENDPOINTS
       "ENVIRONMENT_SISENSE_URL" = "https://sisense.demo.lab.us.azure.sapienceanalytics.com"
+      #"ENVIRONMENT_SISENSE_URL" = "https://sisense-linux.demo.sapienceanalytics.com"
       "ENVIRONMENT_SISENSE_JS_PLATFORM"  =  "windows"
+      #"ENVIRONMENT_SISENSE_JS_PLATFORM"  =  "linux"
 
-      #SISENSE INTERNAL ENDPOINTS
+      #SISENSE WINDOWS INTERNAL ENDPOINTS
       "Sisense__BaseHost" = "http://sisense.demo.lab.red.us.azure.internal.sapienceanalytics.com:8081/"
       "Sisense__SecurityEndpoint" = "http://sisense.demo.lab.red.us.azure.internal.sapienceanalytics.com:8081/api/settings/security"
       "Sisense__UserSecurityEndpoint" = "http://sisense.demo.lab.red.us.azure.internal.sapienceanalytics.com:8081/api/v1/users"
 
+      #SISENSE LINUX INTERNAL ENDPOINTS
+      #"Sisense__BaseHost" = "https://sisense-linux.demo.sapienceanalytics.com/"
+      #"Sisense__SecurityEndpoint" = "https://sisense-linux.demo.sapienceanalytics.com/api/settings/security"
+      #"Sisense__UserSecurityEndpoint" = "https://sisense-linux.demo.sapienceanalytics.com/api/v1/users"
+
       "Sisense__BaseUrl" = "https://sisense.demo.lab.us.azure.sapienceanalytics.com/"
+      #"Sisense__BaseUrl" = "hhttps://sisense-linux.demo.sapienceanalytics.com/"
+
+
   }
 }
