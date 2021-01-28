0566dc05ff0d461cd146b806a473eebd19e260b5
Ashish Gadewar <ashish.gadewar@sapienceanalytics.com>
2021-01-28 11:58:52 +0530

    sisense linux changes

--- realms/azure-lab-red-us/environments/demo/config-maps/sisense.tf
+++ realms/azure-lab-red-us/environments/demo/config-maps/sisense.tf
@@ -9,8 +9,8 @@ resource "kubernetes_config_map" "sisense" {
       "Sisense__Env" = "Demo"
 
       #SISENSE DATASOURCES
-      "Sisense__DataSource" = "Set/Sapience-Daily-CompanyId-Env"
-      #"Sisense__DataSource" = "Sapience-Daily-CompanyId-Env"
+      #"Sisense__DataSource" = "Set/Sapience-Daily-CompanyId-Env"
+      "Sisense__DataSource" = "Sapience-Daily-CompanyId-Env"
       "Sisense_DailyDataSource" = "Sapience-Daily-CompanyId-Env"
       "Sisense_HourlyDataSource" = "Sapience-Hourly-CompanyId-Env"
 
@@ -22,31 +22,31 @@ resource "kubernetes_config_map" "sisense" {
       "Sisense_ElasticubesUri" = "api/v1/elasticubes/getElasticubes"
       "Sisense_PingUri" = "api/test"
       #UPDATE REQUEST URI (e.g. SapienceCube-[Env])
-      "Sisense__RequestUri" = "api/elasticubes/SapienceCube-Demo/jaql"
-      #"Sisense__RequestUri" = "api/datasources/SapienceCube-Dev/jaql"
+      #"Sisense__RequestUri" = "api/elasticubes/SapienceCube-Demo/jaql"
+      "Sisense__RequestUri" = "api/datasources/SapienceCube-Dev/jaql"
       "Sisense_SearchUserUri" = "api/users?search="
       "Sisense_SecurityUri" = "api/settings/security"
       "Sisense_UsersUri" = "api/users?notify=false"
       
 
       #SISENSE EXTERNAL ENDPOINTS
-      "ENVIRONMENT_SISENSE_URL" = "https://sisense.demo.lab.us.azure.sapienceanalytics.com"
-      #"ENVIRONMENT_SISENSE_URL" = "https://sisense-linux.demo.sapienceanalytics.com"
-      "ENVIRONMENT_SISENSE_JS_PLATFORM"  =  "windows"
-      #"ENVIRONMENT_SISENSE_JS_PLATFORM"  =  "linux"
+      #"ENVIRONMENT_SISENSE_URL" = "https://sisense.demo.lab.us.azure.sapienceanalytics.com"
+      "ENVIRONMENT_SISENSE_URL" = "https://sisense-linux.demo.sapienceanalytics.com"
+      #"ENVIRONMENT_SISENSE_JS_PLATFORM"  =  "windows"
+      "ENVIRONMENT_SISENSE_JS_PLATFORM"  =  "linux"
 
       #SISENSE WINDOWS INTERNAL ENDPOINTS
-      "Sisense__BaseHost" = "http://sisense.demo.lab.red.us.azure.internal.sapienceanalytics.com:8081/"
-      "Sisense__SecurityEndpoint" = "http://sisense.demo.lab.red.us.azure.internal.sapienceanalytics.com:8081/api/settings/security"
-      "Sisense__UserSecurityEndpoint" = "http://sisense.demo.lab.red.us.azure.internal.sapienceanalytics.com:8081/api/v1/users"
+      #"Sisense__BaseHost" = "http://sisense.demo.lab.red.us.azure.internal.sapienceanalytics.com:8081/"
+      #"Sisense__SecurityEndpoint" = "http://sisense.demo.lab.red.us.azure.internal.sapienceanalytics.com:8081/api/settings/security"
+      #"Sisense__UserSecurityEndpoint" = "http://sisense.demo.lab.red.us.azure.internal.sapienceanalytics.com:8081/api/v1/users"
 
       #SISENSE LINUX INTERNAL ENDPOINTS
-      #"Sisense__BaseHost" = "https://sisense-linux.demo.sapienceanalytics.com/"
-      #"Sisense__SecurityEndpoint" = "https://sisense-linux.demo.sapienceanalytics.com/api/settings/security"
-      #"Sisense__UserSecurityEndpoint" = "https://sisense-linux.demo.sapienceanalytics.com/api/v1/users"
+      "Sisense__BaseHost" = "https://sisense-linux.demo.sapienceanalytics.com/"
+      "Sisense__SecurityEndpoint" = "https://sisense-linux.demo.sapienceanalytics.com/api/settings/security"
+      "Sisense__UserSecurityEndpoint" = "https://sisense-linux.demo.sapienceanalytics.com/api/v1/users"
 
-      "Sisense__BaseUrl" = "https://sisense.demo.lab.us.azure.sapienceanalytics.com/"
-      #"Sisense__BaseUrl" = "hhttps://sisense-linux.demo.sapienceanalytics.com/"
+      #"Sisense__BaseUrl" = "https://sisense.demo.lab.us.azure.sapienceanalytics.com/"
+      "Sisense__BaseUrl" = "hhttps://sisense-linux.demo.sapienceanalytics.com/"
 
 
   }
--- realms/azure-lab-red-us/environments/demo/config-maps/users.tf
+++ realms/azure-lab-red-us/environments/demo/config-maps/users.tf
@@ -12,7 +12,7 @@ resource "kubernetes_config_map" "users" {
       "AzureServiceBus__EditEndpoint"            = "Endpoint=sb://sapience-lab-us-demo.servicebus.windows.net/;SharedAccessKeyName=Full;SharedAccessKey=UxCI+/JDBtT0rx7jB8af0XLkQRY4J2D0fS+6l7mzekE="
       "AzureServiceBus__DeleteEntityPath"        = "sapience-admin-users-deleted"
       "AzureServiceBus__DeleteEndpoint"          = "Endpoint=sb://sapience-lab-us-demo.servicebus.windows.net/;SharedAccessKeyName=Full;SharedAccessKey=VaCNsnPSvUGW6o2EzxYH8RQxn4TQuZursIVncP3bnOY="
-      "AzureServiceBus__DeletedUsersEndPoint"    = "Endpoint=sb://sapience-lab-us-demo.servicebus.windows.net/;SharedAccessKeyName=Publish;SharedAccessKey=C6j0bHH+E1xWeWWekjrbZd7QKFyjHuixO/lu35b2AtQ=;EntityPath=sapience-admin-users-deleted"
+      "AzureServiceBus__DeletedUsersEndPoint"    = "Endpoint=sb://sapience-lab-us-demo.servicebus.windows.net/;SharedAccessKeyName=Publish;SharedAccessKey=C6j0bHH+E1xWeWWekjrbZd7QKFyjHuixO/lu35b2AtQ="
       "AzureServiceBus__DeletedUsersEntityPath"  = "sapience-admin-users-deleted"
   }
 }
