75684ac228b6caf8c76a8e967e904bf8cf9a3fb4
Ashish <ashish.gadewar@sapienceanalytics.com>
2021-03-31 23:23:00 +0530

    nexus deployment

---
+++ realms/azure-global-us/components/nexus/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  backend "azurerm" {
+    key = "nexus.tfstate"
+  }
+}
+
+provider "kubernetes" {
+  config_path = local.config_path
+}
+
+# provider "helm" {
+#   kubernetes {
+#     config_path = local.config_path
+#   }
+
+  #TODO - may want to pull service account name from kubernetes_service_account.tiller.metadata.0.name
+  #service_account = "tiller"
+#}
+
+locals {
+  config_path = "../kubernetes/.local/kubeconfig"
+  namespace   = "nexus"
+
+  common_tags = merge(
+    var.realm_common_tags,
+    {
+      "Component" = "nexus"
+    },
+  )
+}
+
+# resource "helm_release" "sonatype-nexus" {
+#   name      = "sonatype-nexus"
+#   namespace = "nexus"
+#   chart     = "stable/sonatype-nexus"
+# }
+
+resource "null_resource" "sonatype-nexus" {
+  provisioner "local-exec" {
+    command = "kubectl --kubeconfig=${local.config_path} -n ${local.namespace} apply -f deployment.yaml"
+  }
+}
+
+resource "null_resource" "sonatype-nexus-service" {
+  provisioner "local-exec" {
+    command = "kubectl --kubeconfig=${local.config_path} -n ${local.namespace} apply -f service.yaml"
+  }
+}
