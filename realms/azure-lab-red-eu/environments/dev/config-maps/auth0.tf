7b043d91805801aeab6471b1605b29831bd64198
Shashank <shashank.gadge@sapienceanalytics.com>
2021-06-23 17:52:24 +0530

    auth0 configmap updated

--- realms/azure-lab-red-eu/environments/dev/config-maps/auth0.tf
+++ realms/azure-lab-red-eu/environments/dev/config-maps/auth0.tf
@@ -6,35 +6,35 @@ resource "kubernetes_config_map" "auth0" {
 
   data = {
       #MAIN AUTH0
-      "Auth0__Authority" = "https://login.dev.lab.sapienceanalytics.com/"
+      "Auth0__Authority" = "https://sapience-lab-eu-dev.eu.auth0.com"
       "Auth0__Audience" = "https://api.sapienceanalytics.com"
-      "Auth0__ClientId" = "gEurUe965S21CvJyQtArQ3z8TahgC20K"
+      "Auth0__ClientId" = "BJ6q2sxMtSYvYwCpWteqFyKpoaRc282w"
       "Auth0__Connection" = "Username-Password-Authentication"
       "Auth0__GrantType" = "client_credentials"
 
       #UI AUTH0
-      "ENVIRONMENT_ADMIN_URL"      = "https://manage.dev.sapienceanalytics.com"
-      "ENVIRONMENT_AUTH_AUTHORITY" = "https://login.dev.lab.sapienceanalytics.com"
+      "ENVIRONMENT_ADMIN_URL"      = "https://manage.dev.eu.sapienceanalytics.com"
+      "ENVIRONMENT_AUTH_AUTHORITY" = "https://sapience-lab-eu-dev.eu.auth0.com"
       "ENVIRONMENT_AUTH_AUDIENCE" = "https://api.sapienceanalytics.com"
-      "ENVIRONMENT_AUTH_CLIENT_ID" = "gEurUe965S21CvJyQtArQ3z8TahgC20K"
+      "ENVIRONMENT_AUTH_CLIENT_ID" = "BJ6q2sxMtSYvYwCpWteqFyKpoaRc282w"
       "ENVIRONMENT_AUTH_SCOPE" = "openid email profile"
-      "ENVIRONMENT_VUE_URL" = "https://app.dev.sapienceanalytics.com"
+      "ENVIRONMENT_VUE_URL" = "https://app.dev.lab-eu.sapienceanalytics.com"
 
       #AUTH0 URI
-      "Auth0__PingUri" = "https://login.dev.lab.sapienceanalytics.com/test"
+      "Auth0__PingUri" = "https://sapience-lab-eu-dev.eu.auth0.com/test"
 
       #MANAGEMENT API
-      "Auth0__ManagementApiBaseUrl" = "https://dev-piin5umt.auth0.com"
-      "Auth0__ManagementApiAudience" = "https://dev-piin5umt.auth0.com/api/v2/"
-      "Auth0__ManagementApiClientId" = "pGmGyQ49XNlCp8gd46a2cbEwC53xX4sj"
+      "Auth0__ManagementApiBaseUrl" = "https://sapience-lab-eu-dev.eu.auth0.com"
+      "Auth0__ManagementApiAudience" = "https://sapience-lab-eu-dev.eu.auth0.com/api/v2/"
+      "Auth0__ManagementApiClientId" = "5BIiNYJDqLcNet95rtK5VaQh9IC9Jqa5"
 
       #Open-API
-      "Auth0ManagementApi__BaseUrl"  = "https://dev-piin5umt.auth0.com"
-      "Auth0ManagementApi__Audience" = "https://dev-piin5umt.auth0.com/api/v2/"
-      "Auth0ManagementApi__ClientId" = "pGmGyQ49XNlCp8gd46a2cbEwC53xX4sj"
-      "Auth0ManagementApi__OpenApiAudience" = "https://sapience-lab-us-dev.developer.azure-api.net"
+      "Auth0ManagementApi__BaseUrl"  = "https://sapience-lab-eu-dev.eu.auth0.com"
+      "Auth0ManagementApi__Audience" = "https://sapience-lab-eu-dev.eu.auth0.com/api/v2/"
+      "Auth0ManagementApi__ClientId" = "5BIiNYJDqLcNet95rtK5VaQh9IC9Jqa5"
+      "Auth0ManagementApi__OpenApiAudience" = "https://sapience-lab-eu-dev.developer.azure-api.net"
 
       # POST API NOTIFICATION
-      "auth0_alertrules_id" = "w6Qfd31Vzgv2b1rfqIj9II5rWnTB7HQv"
+      "auth0_alertrules_id" = "Vm3QDRujqkNdmfqKnNdUnFUGz7bTgA2h"
   }
 }
